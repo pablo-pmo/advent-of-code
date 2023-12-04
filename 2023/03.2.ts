5f4b81fde2422452adaf84b037c3c6d2812d5a76
pablo-pmo <impablopmo@gmail.com>
2023-12-04 14:36:13 +0100

    Day 03.2 SOLVED

---
+++ 2023/03.2.ts
@@ -0,0 +1,70 @@
+import { readFileSync } from 'fs';
+import { resolve as path } from 'path';
+import { EOL } from 'os';
+
+const main = () => {
+    let input: string = readFileSync(path(__dirname, './03.input.txt'), { encoding: 'utf8' });
+
+    const c: string = '*';
+
+    input = input.replaceAll(EOL, ',');
+    const width: number = input.indexOf(',');
+
+    let sum: number = 0;
+
+    while (input.includes(c)) {
+
+        const where: number = input.indexOf(c);
+        input = input.replace(c, '.');
+
+        const neighbours: number[] = [
+            where - width - 1 - 1,
+            where - width - 1,
+            where - width + 1 - 1,
+            where - 1,
+            where + 1,
+            where + width - 1 + 1,
+            where + width + 1,
+            where + width + 1 + 1,
+        ];
+
+        let peeks: [number, string][] = neighbours.map((neighbour: number): [number, string] => [neighbour, input[neighbour]]);
+        peeks = peeks.filter(([_, char]: [number, string]): boolean => !isNaN(+char));
+
+        const borders: [number, number][] = [];
+
+        peeks.forEach(([peek, _]: [number, string]) => {
+
+            if (borders.some(([left, right]: [number, number]): boolean => peek >= left && peek <= right)) return;
+
+            const border: [number, number] = [peek, peek];
+            const searching: [boolean, boolean] = [true, true];
+
+            for (let i: number = 1; i < width; i++) {
+                if (searching[0] && !isNaN(+input[peek - i])) {
+                    border[0] = peek - i;
+                } else searching[0] = false;
+                if (searching[1] && !isNaN(+input[peek + i])) {
+                    border[1] = peek + i;
+                } else searching[1] = false;
+                if (!searching[0] && !searching[1]) {
+                    borders.push(border);
+                    return;
+                }
+            }
+
+        });
+
+
+        if (borders.length !== 2) continue;
+
+        sum += borders.reduce((acc: number, [left, right]: [number, number]): number => acc * +input.substring(left, right + 1), 1);
+
+
+    }
+
+    console.log(sum);
+
+};
+
+main();
