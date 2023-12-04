520382596ce9f751de13f17ebed5f78e4ec4940d
pablo-pmo <impablopmo@gmail.com>
2023-12-04 10:09:40 +0100

    Day 02.1 SOLVED

---
+++ 2023/02.1.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from 'fs';
+import { resolve as path } from 'path';
+import { EOL } from 'os';
+
+const main = () => {
+    const input: string = readFileSync(path(__dirname, './02.input.txt'), { encoding: 'utf8' });
+    const lines: string[] = input.split(EOL);
+
+    const colourCount: { [key: string]: number } = { red: 12, green: 13, blue: 14 };
+    const checkColourCount = (line: string) => {
+        const [amount, text] = line.split(' ');
+        return colourCount[text] < +amount;
+    }
+
+    let sum = 0;
+    for (const line of lines) {
+        let [id, plays] = line.split(':');
+        id = id.replace(/Game /, '');
+
+        let illegalPlays: string[] = plays.split('; ').map((play: string) => play.split(', ').filter(checkColourCount)).flat();
+
+        if (illegalPlays.length > 0) continue;
+        sum += +id;
+    }
+    console.log(sum);
+
+};
+
+main();
