46c4122cb83b6e2613e387b455093e74a515e9c8
pablo-pmo <impablopmo@gmail.com>
2023-12-13 16:38:39 +0100

    Day 05.2 filtered unused transformations

--- 2023/05.2.ts
+++ 2023/05.2.ts
@@ -17,17 +17,23 @@ const main = () => {
 
 
 
-    // for (const line of lines) {
+    for (const line of lines) {
 
-    //     const [_, ...transformations]: number[][] = line.split(EOL).map((transformation: string): number[] => transformation.split(' ').map((text: string): number => +text));
+        let [_, ...transformations]: number[][] = line.split(EOL).map((transformation: string): number[] => transformation.split(' ').map((text: string): number => +text));
 
-    //     seeds = seeds.map((seed: number): number => {
-    //         const transformation: number[] | undefined = transformations.filter((transformation: number[]): boolean => (seed >= transformation[1]) && (seed < (transformation[1] + transformation[2]))).shift();
-    //         if (transformation !== undefined) return transformation[0] + seed - transformation[1];
-    //         return seed;
-    //     });
+        console.log(transformations);
 
-    // }
+        transformations = transformations.filter(([_, tStart, tLength]: number[]): boolean => seeds.some(([sStart, sLength]: [number, number]): boolean => ((tStart <= sStart) && (tStart + tLength >= sStart)) || ((sStart <= tStart) && (sStart + sLength >= tStart))));
+
+
+        console.log(transformations);
+        // seeds = seeds.map((seed: number): number => {
+        //     const transformation: number[] | undefined = transformations.filter((transformation: number[]): boolean => (seed >= transformation[1]) && (seed < (transformation[1] + transformation[2]))).shift();
+        //     if (transformation !== undefined) return transformation[0] + seed - transformation[1];
+        //     return seed;
+        // });
+
+    }
     // const min: number = seeds.sort((a: number, b: number): number => a > b ? 1 : -1).shift()!;
     // console.log(min);
 
