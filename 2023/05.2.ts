0d80e6042ac6c118b30db686a745577fd3800de3
pablo-pmo <impablopmo@gmail.com>
2023-12-13 15:23:02 +0100

    Day 05.2 origin translated into ranges

--- 2023/05.2.ts
+++ 2023/05.2.ts
@@ -3,24 +3,34 @@ import { resolve as path } from 'path';
 import { EOL } from 'os';
 
 const main = () => {
-    const input: string = readFileSync(path(__dirname, './05.input.txt'), { encoding: 'utf8' });
+    const input: string = readFileSync(path(__dirname, './05.test.txt'), { encoding: 'utf8' });
     const lines: string[] = input.split(EOL + EOL);
 
-    let seeds: number[] = lines.shift()!.replace('seeds: ', '').split(' ').map((seed: string): number => +seed);
+    let origin: number[] = lines.shift()!.replace('seeds: ', '').split(' ').map((seed: string): number => +seed);
+    let seeds: [number, number][] = [];
 
-    for (const line of lines) {
-
-        const [_, ...transformations]: number[][] = line.split(EOL).map((transformation: string): number[] => transformation.split(' ').map((text: string): number => +text));
-
-        seeds = seeds.map((seed: number): number => {
-            const transformation: number[] | undefined = transformations.filter((transformation: number[]): boolean => (seed >= transformation[1]) && (seed < (transformation[1] + transformation[2]))).shift();
-            if (transformation !== undefined) return transformation[0] + seed - transformation[1];
-            return seed;
-        });
-
+    for (let i: number = 0; i < origin.length; i += 2) {
+        seeds.push([origin[i], origin[i] + origin[i + 1] - 1]);
     }
-    const min: number = seeds.sort((a: number, b: number): number => a > b ? 1 : -1).shift()!;
-    console.log(min);
+
+    console.log(origin);
+    console.log(seeds);
+
+
+
+    // for (const line of lines) {
+
+    //     const [_, ...transformations]: number[][] = line.split(EOL).map((transformation: string): number[] => transformation.split(' ').map((text: string): number => +text));
+
+    //     seeds = seeds.map((seed: number): number => {
+    //         const transformation: number[] | undefined = transformations.filter((transformation: number[]): boolean => (seed >= transformation[1]) && (seed < (transformation[1] + transformation[2]))).shift();
+    //         if (transformation !== undefined) return transformation[0] + seed - transformation[1];
+    //         return seed;
+    //     });
+
+    // }
+    // const min: number = seeds.sort((a: number, b: number): number => a > b ? 1 : -1).shift()!;
+    // console.log(min);
 
 };
 
